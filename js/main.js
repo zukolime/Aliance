150468a0ecdc3c8e5a91138cf0c158dc9b8d8086
zukolime <alena.perminova.1993@gmail.com>
2024-10-22 15:42:14 +0500

    add: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
@@ -9,3 +11,7 @@ window.addEventListener("scroll", () => {
     logo.href.baseVal = "./img/sprite.svg#logo-light";
   }
 });
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.toggle('is-open');
+})
